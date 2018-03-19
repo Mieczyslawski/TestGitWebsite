182b75c2424c286953563e56f6895e4075799008
Piotr Blachnicki <TMK476@motorolasolutions.com>
2018-03-19 15:07:41 +0100

    test

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,11 @@
 console.log("aaa");
+var shoppingList = {
+    ShoppingListName: "My Shopping List",
+    Items: [{name: "Kapusta", checked: true}, {name: "Jabłuszko", checked: false}, {name: "Woda", checked: true}]
+}
+
+var app = new Vue({
+    el: "#app",
+    data: shoppingList
+})
+
